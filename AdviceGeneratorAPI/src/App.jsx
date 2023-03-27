1ea46dc3f487d4348201003440a99536f5f2aa63
Felipe Baldim Guerra <felipe.guerra@ofi.com>
2023-03-27 16:16:31 -0300

    Styles.module

--- AdviceGeneratorAPI/src/App.jsx
+++ AdviceGeneratorAPI/src/App.jsx
@@ -1,9 +1,12 @@
+import React from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import dividor from './assets/pattern-divider-mobile.svg'
+import dice from './assets/icon-dice.svg'
+
+import styles from './styles.module.css'
 
 import './App.css'
+
 
 // Step 1: Break the UI into a component hierarchy
 // Step 2: Build a static version in React
@@ -13,17 +16,59 @@ import './App.css'
 
 
 function App() {
-  
-} 
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <Card />
+    </div>
+    
+  )
+}
 
 // The card, 
+function Card() {
+  return (
+    <div className={styles.card}></div>
+  )
+}
 
 // The title for the card "advice # 117"
+function Title() {
+  return (
+    <div>
+      <p className='title'>Advice # 117</p>
+    </div>
+    )
+}
 
 // The advice -> which will be fetched in the API
+function Advice () {
+  return(
+    <div className='advice'>
+      <p>Try buying a coffee for the creator of a free public API, now and then.</p>
+    </div>
+    )
+}
 
 // The dividor
+function Dividor () {
+  return(
+    <div className='dividor'>
+      <img src={dividor}></img>
+    </div>
+  )
+}
 
 // The button 
+function Button() {
+  return(
+    <div className='button'>
+      <button>{dice}</button>
+    </div>
+  )
+}
+
+
+
 
 export default App

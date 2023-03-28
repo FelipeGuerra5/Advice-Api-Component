eae52d7361443d23f16dc1901c801c422952b39f
Felipe Baldim Guerra <fbaldim.guerra@gmail.com>
2023-03-28 17:01:42 -0300

    Updating Vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/FelipeGuerra5/Advice-Api-Component'
+  base: 'https://FelipeGuerra5.github.io/Advice-Api-Component'
 })

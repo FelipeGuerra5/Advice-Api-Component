2a5414e33b1290a43c82074ea630ac7d13f65869
Felipe Baldim Guerra <fbaldim.guerra@gmail.com>
2023-03-28 15:18:32 -0300

    First Working Version
    Important
    1. Handles loop and starts again
    2. If there one failed message on the middle of the counter it can handle it
    3. The counter for the messages is stored in the LocalStorage
    Now Deploy on GitHub Pages

--- AdviceGeneratorAPI/src/App.jsx
+++ AdviceGeneratorAPI/src/App.jsx
@@ -1,56 +1,74 @@
 import React from 'react'
-import { useState } from 'react'
-import mobileDividor from './assets/pattern-divider-mobile.svg'
-import deskDividor from './assets/pattern-divider-desktop.svg'
+import { useState, useEffect } from 'react'
 import dice from './assets/icon-dice.svg'
-
-import './App.css'
+import axios from 'axios'
 import styles from './styles.module.css'
+import './App.css'
 
-
-
-// Step 1: Break the UI into a component hierarchy
-// Step 2: Build a static version in React
-// Step 3: Find the minimal but complete representation of UI state
-// Step 4: Identify where your state should live
-// Step 5: Add inverse data flow
-
+const localCounter = localStorage.getItem("counter")
+if (localCounter) {
+  console.log(localCounter)
+}
 
 function App() {
+  const [counter, setCounter] = useState(Number(localCounter ? localCounter: 1))
+  const [advice, setAdvice] = useState()
+
+  
+  useEffect(() => {
+    axios
+    .get(`https://api.adviceslip.com/advice/${counter}`)
+    .then(response => {
+      if (response.data.slip) {
+        setAdvice(response.data.slip.advice)
+        localStorage.setItem("counter", counter)
+      } if (response.data.message) {
+        // setCounter(1) -> Should be this, 
+        // however the API does not return
+        // at the ID: 22 so:
+        counter == 225 ? setCounter(1) : setCounter( counter += 1) 
+      }
+    })
+    .catch(err => console.error(err))
+  },[counter])
+  
   return (
-    <div>
-      <Card />
-    </div>
-    
+    <Card 
+      counter={counter} 
+      setCounter={setCounter}
+      advice={advice} 
+      />
   )
 }
 
 // The card, 
-function Card() {
+function Card({ counter, advice, setCounter }) {
   return (
     <div className={styles.card}>
-      <Title />
-      <Advice />
+      <Title counter={counter} />
+      <Advice advice={advice} />
       <Dividor />
-      <Button />
+      <Button counter={counter} setCounter={setCounter} />
     </div>
   )
 }
 
 // The title for the card "advice # 117"
-function Title() {
+function Title({ counter }) {
+  
+  const title = `ADVICE #${counter}`
   return (
     <div>
-      <p className={styles.title} >ADVICE #117</p>
+      <p className={styles.title} >{title}</p>
     </div>
     )
 }
 
 // The advice -> which will be fetched in the API
-function Advice () {
+function Advice ({ advice }) {
   return(
     <div className={styles.advice} >
-      <p className={styles.adviceP} >"It's easy to sit up and take notice what's difficult is getting up and taking action."</p>
+      <p className={styles.adviceP} >"{advice}"</p>
     </div>
     )
 }
@@ -65,17 +83,14 @@ function Dividor () {
 }
 
 // The button 
-function Button() {
+function Button({ counter, setCounter }) {
   return(
-    <div className={styles.shadow}>
+    <div className={styles.shadow} onClick={(e)=>setCounter(counter += 1)} >
       <div className={styles.next} >
         <img src={dice} className={dice}/>
       </div>
     </div>
   )
 }
-
-
-
 
 export default App
